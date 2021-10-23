c0b0363586d9baafcf71fbe57dcbce54ad975e73
woshihaozhu <358975248@qq.com>
2021-10-23 08:58:47 +0800

    import contraloss

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .wav2lip import Wav2Lip, Wav2Lip_disc_qual, Wav2Lip_ori
 from .syncnet import SyncNet_color
 from .vgg.vgg19 import VGG19
-from .loss import VGGLoss, Sync_loss
+from .loss import VGGLoss, Sync_loss, Contrastive_loss
 from .arcface.iresnet import iresnet18, iresnet34, iresnet50
 from .hier_model import HModel
